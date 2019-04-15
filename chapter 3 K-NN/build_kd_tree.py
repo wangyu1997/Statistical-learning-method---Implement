81db2405661d28b748cf781d0d4c335d6c01bb51
wangyu1997 <wangyu19970819@qq.com>
2019-04-15 21:46:55 +0800

    find nearest bug to be fixed

--- chapter 3 K-NN/build_kd_tree.py
+++ chapter 3 K-NN/build_kd_tree.py
@@ -1,10 +1,17 @@
 import numpy as np
 
+
 class KdNode:
     def __init__(self, value=None, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
+        self.parent = None
+        self.visited = False
+
+    def clean_visit(self):
+        self.visited = False
+
 
 class KdTree:
     def __init__(self, data=np.array([])):
@@ -13,7 +20,7 @@ class KdTree:
         self.N = len(data[0])
         self.kd_tree = self.build(0, self.size, 0)
 
-    def build(self, left, right, index):
+    def build(self, left, right, index, parent=None):
         if left >= right:
             return None
         elif right == left + 1:
@@ -22,16 +29,46 @@ class KdTree:
             self.data[left:right] = \
                 self.data[left:right][self.data[left:right, index].argsort()]
             mid = int((right + left) / 2)
-            return KdNode(value=self.data[mid],
-                          left=self.build(left, mid, (index + 1) % self.N),
-                          right=self.build(mid + 1, right, (index + 1) % self.N))
+            node = KdNode(value=self.data[mid])
+            node.parent = parent
+            node.left = self.build(left, mid, (index + 1) % self.N, node)
+            node.right = self.build(mid + 1, right, (index + 1) % self.N, node)
+            return node
 
     def mid_tra(self):
-        self._mid_travel(self.kd_tree)
+        self._mid_travel(travel=lambda node: print("value = ", node.value), node=self.kd_tree)
 
-    def _mid_travel(self, node=None):
+    def _mid_travel(self, travel, node=None):
         if node is not None:
-            print("value = ", node.value)
-            self._mid_travel(node.left)
-            self._mid_travel(node.right)
+            travel(node)
+            self._mid_travel(travel, node.left)
+            self._mid_travel(travel, node.right)
 
+    def search_nearest(self, curr_node, value, distance):
+        dis = distance(value, curr_node.value)
+        curr_node.visited = True
+        if curr_node.parent is not None:
+            p_dis = distance(value, curr_node.parent.value)
+
+    def find_leaf_node(self, value=None):
+        return self._find_leaf_node(self.kd_tree, search_node=KdNode(value=value), index=0)
+
+    def _find_leaf_node(self, curr_node, search_node, index):
+        search_value = search_node.value
+        if curr_node.left is None:
+            return curr_node
+        if search_value[index] <= curr_node.value[index]:
+            return self._find_leaf_node(curr_node.left, search_node, (index + 1) % self.N)
+        elif curr_node.right is None:
+            return curr_node
+        else:
+            return curr_node.right
+
+    def clean_visited_state(self):
+        self._mid_travel(travel=lambda node: node.clean_visit(node), node=self.kd_tree)
+
+
+data = np.array([[2, 3], [5, 4], [9, 6], [4, 7], [8, 1], [7, 2]])
+tree = KdTree(data)
+tree.mid_tra()
+print(tree.find_leaf_node([8.1, 9]).value)
